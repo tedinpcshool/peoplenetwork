deb5dce8d598f7443d0253ef9dd8b3b88b3f52f6
tedinpcschool <ted2012ios@gmail.com>
2017-05-31 21:18:59 +0800

    update files

--- peoplenetwork/peoplenetwork/peoplenetwork/ViewController.swift
+++ peoplenetwork/peoplenetwork/peoplenetwork/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.writeFile()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+    }
+    
 
+    func writeFile(){
+        
+        let file = "file.txt" //this is the file. we will write to and read from it
+        let text = "some text" //just a text
+//        FileManager.default.urls(for: <#T##FileManager.SearchPathDirectory#>, in: <#T##FileManager.SearchPathDomainMask#>)
+        // optional binding
+        
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            print("dir=\(dir)")
+            let path = dir.appendingPathComponent(file)
+            //writing
+            do {
+                try text.write(to: path, atomically: false, encoding: String.Encoding.utf8)
+            }
+            catch {/* error handling here */}
+                print("path=\(path)")
+                //reading
+                do {
+                    let text2 = try String(contentsOf: path, encoding: String.Encoding.utf8)
+                    print("text2 = \(text2)")
+                }
+                catch {
+                    /* error handling here */
+            
+            }
+        }
+    }
+    
 }
 
